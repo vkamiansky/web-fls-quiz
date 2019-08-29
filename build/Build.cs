2cdb39cb9c1c8c7f4c08471b00d0c723362dc101
Vitaly Kamiansky <vitaly.kamiansky@firstlinesoftware.com>
2019-08-29 13:27:45 +0300

    Added docker build/push scenarios

--- build/Build.cs
+++ build/Build.cs
@@ -1,17 +1,12 @@
-using System;
-using System.IO;
-using System.Linq;
 using Nuke.Docker;
 using Nuke.Common;
 using Nuke.Common.Execution;
 using Nuke.Common.Git;
 using Nuke.Common.ProjectModel;
-using Nuke.Common.Tooling;
 using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Tools.GitVersion;
 using Nuke.Common.Utilities.Collections;
 using Nuke.Common.BuildServers;
-using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
@@ -23,32 +18,27 @@ namespace Build
     [UnsetVisualStudioEnvironmentVariables]
     class BuildRoot : NukeBuild
     {
-        /// Support plugins are available for:
-        ///   - JetBrains ReSharper        https://nuke.build/resharper
-        ///   - JetBrains Rider            https://nuke.build/rider
-        ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
-        ///   - Microsoft VSCode           https://nuke.build/vscode
-
         public static int Main() => Execute<BuildRoot>(x => x.Compile);
 
         [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
         readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+
+        [Parameter] readonly string DockerUser;
+        [Parameter] readonly string DockerPass;
 
         [Solution] readonly Solution Solution;
         [GitRepository] readonly GitRepository GitRepository;
         [GitVersion] readonly GitVersion GitVersion;
 
         AbsolutePath SourceDirectory => RootDirectory / "src";
-        AbsolutePath TestsDirectory => RootDirectory / "tests";
-        AbsolutePath OutputDirectory => RootDirectory / "output";
+
+        readonly string DockerImageName = "vkamiansky/flsquiz";
 
         Target Clean => _ => _
             .Before(Restore)
             .Executes(() =>
             {
                 SourceDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
-                TestsDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
-                EnsureCleanDirectory(OutputDirectory);
             });
 
         Target MakeBundle => _ => _
@@ -70,6 +60,7 @@ namespace Build
             });
 
         Target Compile => _ => _
+            .OnlyWhenDynamic(() => IsLocalBuild || !AppVeyor.Instance.RepositoryTag)
             .DependsOn(Restore)
             .DependsOn(MakeBundle)
             .Executes(() =>
@@ -84,14 +75,32 @@ namespace Build
             });
 
         Target BuildDockerImage => _ => _
+            .OnlyWhenDynamic(() => IsLocalBuild || AppVeyor.Instance.RepositoryTag)
             .DependsOn(MakeBundle)
             .Executes(() =>
             {
                 DockerBuild(x => x
-                    .SetPath("./src/web-fls-quiz")
+                    .SetPath(SourceDirectory / "web-fls-quiz")
                     .SetFile(SourceDirectory / "web-fls-quiz" / "Dockerfile")
-                    .SetTag("vkamiansky/flsquiz")
+                    .SetTag(DockerImageName + ":" + AppVeyor.Instance.RepositoryTagName)
                 );
+            });
+
+        Target PublishDockerImage => _ => _
+            .OnlyWhenDynamic(() => IsLocalBuild || AppVeyor.Instance.RepositoryTag,
+                             () => !string.IsNullOrWhiteSpace(DockerUser),
+                             () => !string.IsNullOrWhiteSpace(DockerPass))
+            .DependsOn(BuildDockerImage)
+            .Executes(() =>
+            {
+                DockerLogin(x => x
+                    .SetUsername(DockerUser)
+                    .SetPassword(DockerPass)
+                    );
+
+                DockerPush(x => x
+                    .SetName(DockerImageName + ":" + AppVeyor.Instance.RepositoryTagName)
+                    );
             });
     }
 }
